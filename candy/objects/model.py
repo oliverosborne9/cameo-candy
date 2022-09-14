f9053fee7d20ffc44c7272afabc9277f4559af98
oliverosborne9 <oliver@recycleye.com>
2022-09-14 23:49:42 +0100

    feat: delete image file once its record has been deleted in database

--- candy/objects/model.py
+++ candy/objects/model.py
@@ -1,5 +1,8 @@
+import os
+
 from flask_login import LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -16,3 +19,15 @@ class Images(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     datetime = db.Column(db.DateTime)
     img_loc = db.Column(db.String(80))
+
+
+def image_cleanup(mapper, connection, target: Images):
+    """
+    Callback function to delete an image file
+    after its row is removed from the Images table
+    in the database.
+    """
+    os.unlink(target.img_loc)
+
+
+event.listen(Images, "after_delete", image_cleanup)
